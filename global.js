6830546d1943525f12ac8e24dbb03b0cf7b2d455
Vivian Lin <vivianlinnn260@gmail.com>
2025-02-17 23:40:20 -0800

    dynamic graph

--- global.js
+++ global.js
@@ -2,20 +2,44 @@
 const body_width = 270;  
 const body_height = 540; 
 const pointSize = 12; 
-const hoverRadius = 30; 
+const hoverRadius = 20; 
+let processedData = [];
 
-// values to plot point anatomically
 
-//Old absolute positions
-// const points = [
-//     { id: 1, x: 399, y: 563, name: "Colorectal" },        // lower abdomen
-//     { id: 2, x: 399, y: 375, name: "Stomach" },           // upper abdomen
-//     { id: 3, x: 439, y: 438, name: "Biliary/Pancreas" },  // right upper abdomen
-//     { id: 4, x: 399, y: 500, name: "Vascular" },          // central/left abdomen
-//     { id: 5, x: 299, y: 313, name: "Breast" },            // left chest
-//     { id: 6, x: 359, y: 400, name: "Hepatic" },           // right upper abdomen (liver)
-//     { id: 7, x: 399, y: 225, name: "Thyroid" }            // neck
-// ];
+let selectedSex = 'all';
+let selectedLocation = null;
+let clickedLocation;
+let totalCases = 0;
+let maxCount = 0; // Store the maximum count across all possible filters
+
+
+// button for gender
+
+const colorMap = {
+    'all': '#a6a6a6',
+    'female': '#ffaafa',
+    'male': '#9bd5ec'
+};
+
+const buttons = [
+    { id: 'all-button', text: 'All', value: 'all' },
+    { id: 'female-button', text: 'Female', value: 'female' },
+    { id: 'male-button', text: 'Male', value: 'male' }
+];
+ const buttonContainer = d3.select('.button-container');
+ 
+ const filterButtons = buttonContainer
+     .selectAll('.filter-button')
+     .data(buttons)
+     .enter()
+     .append('button')
+     .attr('id', d => d.id)
+     .attr('class', 'filter-button')
+     .classed('selected', d => d.value === selectedSex)
+     .text(d => d.text);
+
+// get tooltip element
+const tooltip = d3.select("#tooltip");
 
 
 //Converted to relative position
@@ -35,20 +59,25 @@ const selectedPoints = new Set();
 let activePoint = null;
 
 // create SVG for silhouette
-const body_svg = d3.select("#silhouette")
+let body_svg;
+
+function silhouette_img(){
+    body_svg = d3.select("#silhouette")
     .append("svg")
     .attr("width", body_width)
     .attr("height", body_height);
 
-// add the silhouette image
-body_svg.append("image")
+    body_svg.append("image")
     .attr("href", "images/male_figure.png") // path to  silhouette image --> WOULD NEED A VARIABLE TO CHANGE
     .attr("width", body_width)
     .attr("height", body_height)
     .attr("preserveAspectRatio", "xMidYMid meet");
+}
 
-// create points
-const diamonds = body_svg.selectAll(".point")
+let diamonds;
+
+function diamond_func() {
+    diamonds = body_svg.selectAll(".point")
     .data(points)
     .enter()
     .append("path")
@@ -59,12 +88,13 @@ const diamonds = body_svg.selectAll(".point")
     .attr("fill", "#000000") // black by default
     .attr("stroke", "#444444")
     .attr("stroke-width", 1.5);
+}
 
-// get tooltip element
-const tooltip = d3.select("#tooltip");
+let hoverAreas;
 
+function hoverfunc(data) {
 // larger area for easier hovering
-const hoverAreas = body_svg.selectAll(".hover-area")
+ hoverAreas = body_svg.selectAll(".hover-area")
     .data(points)
     .enter()
     .append("circle")
@@ -82,12 +112,16 @@ const hoverAreas = body_svg.selectAll(".hover-area")
                 .attr("stroke", "#CC0000");
         }
 
-        // show tooltip regardless
-        tooltip
-        .style("opacity", 1)
-        .html(d.name)
-        .style("left", (d.x + 15) + "px")
-        .style("top", (d.y - 15) + "px");
+        selectedLocation = d.name;
+         // show tooltip regardless
+         tooltip
+         .style("opacity", 1)
+         .html(d.name)
+         .style("left", (d.x + 15) + "px")
+         .style("top", (d.y - 15) + "px");
+        //dynamically change histogram and aggregates
+        create_graph(data);
+        
     })
     .on("mouseout", function(event, d) {
         // reset to black if not the active point
@@ -102,8 +136,21 @@ const hoverAreas = body_svg.selectAll(".hover-area")
             .attr("stroke", "#CC0000");
         }
 
-        // hide tooltip
-        tooltip.style("opacity", 0);
+         // show tooltip regardless
+         if (clickedLocation) {
+            tooltip
+            .style("opacity", 1)
+            .html(clickedLocation)
+            .style("left", (d.x + 15) + "px")
+            .style("top", (d.y - 15) + "px");
+            selectedLocation = clickedLocation;
+            create_graph(data);
+         } else {
+            tooltip.style("opacity", 0);
+            selectedLocation = null;
+            create_graph(data);
+         }
+
     })
     .on("click", function(event, d) {
         // if clicking on the currently active point, deselect it
@@ -115,6 +162,9 @@ const hoverAreas = body_svg.selectAll(".hover-area")
 
             // clear the active point and plot
             activePoint = null;
+            clickedLocation = null;
+            tooltip.style("opacity", 0);
+
             clearPlot();
     } else {
         // if there's already an active point, reset its color
@@ -122,6 +172,8 @@ const hoverAreas = body_svg.selectAll(".hover-area")
             d3.select(diamonds.nodes()[activePoint-1])
                 .attr("fill", "#000000")
                 .attr("stroke", "#444444");
+            clickedLocation = null;
+            tooltip.style("opacity", 0);
         }
 
         // set this as the new active point
@@ -132,11 +184,43 @@ const hoverAreas = body_svg.selectAll(".hover-area")
             .attr("fill", "#FF0000")
             .attr("stroke", "#CC0000");
 
+        clickedLocation = d.name;
+        tooltip
+        .style("opacity", 1)
+        .html(clickedLocation)
+        .style("left", (d.x + 15) + "px")
+        .style("top", (d.y - 15) + "px");
+
         // show its plot
         showPlot(d);
     }
 });
-    
+}
+
+ // Configuration with increased margins for axis labels
+ const margin = {top: 20, right: 180, bottom: 60, left: 80};
+ const width = 800 - margin.left - margin.right;
+ const height = 400 - margin.top - margin.bottom;
+ 
+ // Define bins
+ const bins = [
+     { label: '0-1', min: 0, max: 1 },
+     { label: '2-3', min: 2, max: 3 },
+     { label: '4-5', min: 4, max: 5 },
+     { label: '6-7', min: 6, max: 7 },
+     { label: '8-9', min: 8, max: 9 },
+     { label: '10-11', min: 10, max: 11 },
+     { label: '12-13', min: 12, max: 13 },
+     { label: '14+', min: 14, max: Infinity }
+ ];
+
+ const svg = d3.select("#chart")
+ .append("svg")
+ .attr("width", width + margin.left + margin.right)
+ .attr("height", height + margin.top + margin.bottom)
+ .append("g")
+ .attr("transform", `translate(${margin.left},${margin.top})`);
+
 // function to show plot for a point --> PLACEHOLDER
 function showPlot(point) {
     const plotContainer = d3.select("#plot");
@@ -160,44 +244,11 @@ function showPlot(point) {
     // plotFunction(point.id, plotSvg);
 }
 
+
 // function to clear the plot
 function clearPlot() {
     d3.select("#plot").html('<p id="plot-message">Click on a point to see details</p>');
 }
-
- // Configuration with increased margins for axis labels
- const margin = {top: 20, right: 180, bottom: 60, left: 80};
- const width = 800 - margin.left - margin.right;
- const height = 400 - margin.top - margin.bottom;
- 
- // Define bins
- const bins = [
-     { label: '0-1', min: 0, max: 1 },
-     { label: '2-3', min: 2, max: 3 },
-     { label: '4-5', min: 4, max: 5 },
-     { label: '6-7', min: 6, max: 7 },
-     { label: '8-9', min: 8, max: 9 },
-     { label: '10-11', min: 10, max: 11 },
-     { label: '12-13', min: 12, max: 13 },
-     { label: '14+', min: 14, max: Infinity }
- ];
-
- const colorMap = {
-     'all': '#a6a6a6',
-     'female': '#ffaafa',
-     'male': '#9bd5ec'
- };
-
- const buttons = [
-     { id: 'all-button', text: 'All', value: 'all' },
-     { id: 'female-button', text: 'Female', value: 'female' },
-     { id: 'male-button', text: 'Male', value: 'male' }
- ];
-
- let selectedSex = 'all';
- let selectedLocation = null;
- let totalCases = 0;
- let maxCount = 0; // Store the maximum count across all possible filters
 
  function show_body(gender) {
     console.log("Showing new gender:");
@@ -222,37 +273,6 @@ function repositionDiamonds() {
         .attr("cy", d => d.yRatio * body_height);
 }
 
-//Old show body
-//  function show_body(gender) {
-//     const body_image = d3.select('#body');
-//     body_image.selectAll('*').remove();
-//     body_image.append('img')
-//     .attr('src', `./images/final_${gender}_figure.png`)
-//     .attr('alt', 'Description of image')
-//     .attr('height', body_height)
-//     .attr('width', body_width);
-//  }
- 
-show_body(selectedSex);
- 
- const svg = d3.select("#chart")
-     .append("svg")
-     .attr("width", width + margin.left + margin.right)
-     .attr("height", height + margin.top + margin.bottom)
-     .append("g")
-     .attr("transform", `translate(${margin.left},${margin.top})`);
-
- const buttonContainer = d3.select('.button-container');
- 
- const filterButtons = buttonContainer
-     .selectAll('.filter-button')
-     .data(buttons)
-     .enter()
-     .append('button')
-     .attr('id', d => d.id)
-     .attr('class', 'filter-button')
-     .classed('selected', d => d.value === selectedSex)
-     .text(d => d.text);
 
  function processCsvData(data) {
      return data.map(d => {
@@ -292,17 +312,26 @@ show_body(selectedSex);
      return globalMax;
  }
 
+ let statsGroup;
+
  function create_graph(data) {
      totalCases = data.length;
      
      let filteredData = data;
+     
      if (selectedSex !== 'all') {
          filteredData = filteredData.filter(d => d.sex === selectedSex[0].toUpperCase());
      }
+
+     const genderTotal = filteredData.length;
+    //  filteredData.forEach(d => console.log(d.department));
+     
      if (selectedLocation) {
-         filteredData = filteredData.filter(d => d.department === selectedLocation);
+         filteredData = filteredData.filter(d => d.optype === selectedLocation);
      }
 
+     console.log(selectedLocation)
+     
      const histogram = bins.map(bin => ({
          binRange: bin.label,
          count: filteredData.filter(d => d.stayDays >= bin.min && d.stayDays <= bin.max).length
@@ -311,6 +340,7 @@ show_body(selectedSex);
      const average = d3.mean(filteredData, d => d.stayDays);
      const filteredTotal = filteredData.length;
      const percentage = ((filteredTotal / totalCases) * 100).toFixed(1);
+     const gender_pct = ((filteredTotal/genderTotal) * 100).toFixed(1);
 
      const x = d3.scaleBand()
          .range([0, width])
@@ -358,7 +388,7 @@ show_body(selectedSex);
          .attr("height", d => height - y(d.count))
          .attr("fill", colorMap[selectedSex]);
 
-     const statsGroup = svg.append("g")
+    statsGroup = svg.append("g")
          .attr("transform", `translate(${width + 10}, 20)`);
 
      statsGroup.append("text")
@@ -374,20 +404,35 @@ show_body(selectedSex);
      statsGroup.append("text")
          .attr("class", "stats-label")
          .attr("y", 50)
-         .text(`(${percentage}% of all cases)`);
+         .text(`${percentage}% of all cases`);
+
+    if (selectedSex !== 'all') {
+        statsGroup.append("text")
+        .attr("class", "stats-label")
+        .attr("y", 75)
+        .text(`${gender_pct}% of ${selectedSex} cases`);
+    }
+     
  }
 
- // Load data and initialize
- d3.csv("clinical_info.csv").then(data => {
-     const processedData = processCsvData(data);
-     maxCount = calculateMaxCount(processedData); // Calculate global maximum
-     create_graph(processedData);
+  // Load data and initialize
+  d3.csv("clinical_info.csv").then(data => {
+    processedData = processCsvData(data);
+    maxCount = calculateMaxCount(processedData); // Calculate global maximum
+    create_graph(processedData);
 
-     // Add click event listeners to buttons
-     filterButtons.on('click', function(event, d) {
-         selectedSex = d.value;
-         filterButtons.classed('selected', btn => btn.value === selectedSex);
-         create_graph(processedData);
-         show_body(selectedSex);
-     });
- });
+    // Add click event listeners to buttons
+    filterButtons.on('click', function(event, d) {
+        selectedSex = d.value;
+        filterButtons.classed('selected', btn => btn.value === selectedSex);
+        create_graph(processedData);
+        show_body(selectedSex);
+    });
+    
+    silhouette_img();
+    diamond_func();
+    hoverfunc(processedData);
+    show_body(selectedSex);
+
+});
+
